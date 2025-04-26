d721b9c80c8ff213f5d6c6ba88182c092b47d9cb
shykevin <2636435735@qq.com>
2025-04-26 17:37:40 +0800

    点评第二次提交，实现优惠卷秒杀功能，具体有生成订单全局唯一ID，优惠卷的添加与下单，乐观锁解决库存超卖问题，实现单服务器下的一人一单问题

---
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,34 @@
+package com.hmdp.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class RedisIdWorker {
+
+    private static  final long BEGIN_TIMESTAMP = 1640995200L;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    public long nextId(String keyprefix){
+        // 生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long timestamp = now.toEpochSecond(ZoneOffset.UTC) - BEGIN_TIMESTAMP;
+
+        // 生成序列号
+        // 获取当前日期
+        String localDate = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        long count = stringRedisTemplate.opsForValue().increment("icr:" + keyprefix + ":" + localDate);
+
+        // 拼接返回
+        return timestamp << 32 | count;   // timestamp向左移32位，在将count与其或运算
+    }
+
+}
